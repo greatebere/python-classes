5ba2f97e4a632fbb719af1eac0d8a01d908150dc
greatebere <greatemebo@gmail.com>
2024-06-22 20:09:52 -0700

    great
    great

---
+++ assingment/assingment/science class .py
@@ -0,0 +1,45 @@
+# this program is to help you known no of student studying a particuler course
+# 1-30 = mass come 
+# 40-60= computer sci
+# 70-100 = civil science
+# 150-200 = computer eginering
+# 210-270 = computer education
+# 275-300 = bio chemistry
+# 320-370 = agric tech
+# 375-400 = ict
+# 410-470 = law
+# 475-500 = political science
+
+
+
+
+print("hello welcome") 
+students = input("enter your no of student ")
+students =int(students)
+if 1 <=  students <=30:
+    print("mass come")
+elif 40 <= students <= 60 :
+    print("cmputer sci")
+elif 70<=  students  <= 100 :
+     print("civil enginering")
+elif 210 <=  students <= 270 :
+     print("computer education")
+elif 275 <=  students <= 300:
+     print("bio chemistry")
+elif 410 <= students <= 470:
+     print("law")
+elif 475 <= students <= 500:
+     print("political science")
+
+else:
+     print("thank you")
+
+
+
+    
+ 
+
+
+
+         
+    
